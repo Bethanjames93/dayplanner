a6411dae1d1a3ec0bbea46f2ef29bf936a6cab51
Bethan James <bethanjames@Bethans-MacBook-Pro.local>
2019-12-01 22:15:18 +0000

    Read Me

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dayplanner
+
+Create a day planner thats lets the user save the events of each hour of the day. 
+
+The application will allow the user to input an activity next to the timeslot and save it to the local storage.
+
+The planner will display the date at the top of the screen the underneath will give an hour by hour schedule that will give the user the oportunity to add their activities then save them. 
--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     });
 
     function hour() {
-        var currentHour = moment().hours();
+        // var currentHour = moment().hour();
       
         $(".time").each(function() {
             var hourBlock = parseInt($(this).attr("id").split("-")[1]);
@@ -31,3 +31,23 @@ $(document).ready(function() {
             }
         });
       }
+
+      hour();
+
+      var interval = setInterval(hour, 15000);
+
+      $("#9 .activity").val(localStorage.getItem("9"));
+      $("#10 .activity").val(localStorage.getItem("10"));
+      $("#11 .activity").val(localStorage.getItem("11"));
+      $("#12 .activity").val(localStorage.getItem("12"));
+      $("#13 .activity").val(localStorage.getItem("13"));
+      $("#14 .activity").val(localStorage.getItem("14"));
+      $("#15 .activity").val(localStorage.getItem("15"));
+      $("#16 .activity").val(localStorage.getItem("16"));
+      $("#17 .activity").val(localStorage.getItem("17"));
+
+      $("#Date").text(moment().format("dddd, MMMM Do"));
+
+      interval();
+
+      });
